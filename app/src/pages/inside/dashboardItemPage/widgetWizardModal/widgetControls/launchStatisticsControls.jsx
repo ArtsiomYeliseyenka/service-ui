26201b95b7fda037f57427b49c0b3a0f6b909d38
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2018-08-06 11:50:14 +0300

    Launch statistics. Second step controls connected to form state 2.

--- app/src/pages/inside/dashboardItemPage/widgetWizardModal/widgetControls/launchStatisticsControls.jsx
+++ app/src/pages/inside/dashboardItemPage/widgetWizardModal/widgetControls/launchStatisticsControls.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { initialize } from 'redux-form';
+import { getFormValues, initialize } from 'redux-form';
 import { defectTypesSelector } from 'controllers/project';
 import { FieldProvider } from 'components/fields/fieldProvider';
 import { injectIntl, defineMessages, intlShape } from 'react-intl';
@@ -22,6 +22,7 @@ import {
 } from './controls';
 import { WIDGET_WIZARD_FORM } from '../widgetWizardContent/wizardControlsSection/constants';
 
+const DEFAULT_ITEMS_COUNT = '50';
 const messages = defineMessages({
   CriteriaFiledLabel: {
     id: 'LaunchStatisticsControls.CriteriaFiledLabel',
@@ -40,16 +41,19 @@ const messages = defineMessages({
 @injectIntl
 @connect(
   (state) => ({
+    widgetSettings: getFormValues(WIDGET_WIZARD_FORM)(state),
     defectTypes: defectTypesSelector(state),
   }),
   {
-    initializeWizardSecondStepForm: (data) => initialize(WIDGET_WIZARD_FORM, data, true),
+    initializeWizardSecondStepForm: (data) =>
+      initialize(WIDGET_WIZARD_FORM, data, true, { keepValues: true }),
   },
 )
 export class LaunchStatisticsControls extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
     defectTypes: PropTypes.object.isRequired,
+    widgetSettings: PropTypes.object.isRequired,
     initializeWizardSecondStepForm: PropTypes.func.isRequired,
   };
 
@@ -61,11 +65,15 @@ export class LaunchStatisticsControls extends Component {
       props.intl.formatMessage,
     );
     props.initializeWizardSecondStepForm({
-      criteria: this.criteria.map((criteria) => criteria.value),
-      items: '10',
-      mode: CHART_MODES.LAUNCH_MODE,
-      viewMode: CHART_MODES.AREA_VIEW,
-      zoom: false,
+      criteria: props.widgetSettings.criteria
+        ? props.widgetSettings.criteria
+        : this.criteria.map((criteria) => criteria.value),
+      items: props.widgetSettings.items ? props.widgetSettings.items : DEFAULT_ITEMS_COUNT,
+      mode: props.widgetSettings.mode ? props.widgetSettings.mode : CHART_MODES.LAUNCH_MODE,
+      viewMode: props.widgetSettings.viewMode
+        ? props.widgetSettings.viewMode
+        : CHART_MODES.AREA_VIEW,
+      zoom: props.widgetSettings.zoom ? props.widgetSettings.zoom : false,
     });
   }
 
