64175aa3c2c623c99804a79a350000088854b022
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2018-08-06 17:38:17 +0300

    Added Investigated percentage of launches chart controls.

--- app/src/pages/inside/dashboardItemPage/widgetWizardModal/widgetControls/investigatedPercentageOfLaunchesControls.jsx
+++ app/src/pages/inside/dashboardItemPage/widgetWizardModal/widgetControls/investigatedPercentageOfLaunchesControls.jsx
@@ -1,5 +1,70 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getFormValues, initialize } from 'redux-form';
+import { FieldProvider } from 'components/fields/fieldProvider';
+import { injectIntl, defineMessages, intlShape } from 'react-intl';
+import { getWidgetModeOptions } from './utils/getWidgetModeOptions';
+import { ITEMS_INPUT_WIDTH, CHART_MODES } from './constants';
+import { FiltersControl, InputControl, TogglerControl } from './controls';
+import { WIDGET_WIZARD_FORM } from '../widgetWizardContent/wizardControlsSection/constants';
 
-export const InvestigatedPercentageOfLaunchesControls = () => (
-  <Fragment>InvestigatedPercentageOfLaunchesControls</Fragment>
-);
+const DEFAULT_ITEMS_COUNT = '50';
+const messages = defineMessages({
+  ItemsFiledLabel: {
+    id: 'InvestigatedPercentageOfLaunchesControls.ItemsFiledLabel',
+    defaultMessage: 'Items',
+  },
+});
+
+@injectIntl
+@connect(
+  (state) => ({
+    widgetSettings: getFormValues(WIDGET_WIZARD_FORM)(state),
+  }),
+  {
+    initializeWizardSecondStepForm: (data) =>
+      initialize(WIDGET_WIZARD_FORM, data, true, { keepValues: true }),
+  },
+)
+export class InvestigatedPercentageOfLaunchesControls extends Component {
+  static propTypes = {
+    intl: intlShape.isRequired,
+    widgetSettings: PropTypes.object.isRequired,
+    initializeWizardSecondStepForm: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    const { widgetSettings, initializeWizardSecondStepForm } = props;
+    initializeWizardSecondStepForm({
+      items: widgetSettings.items ? widgetSettings.items : DEFAULT_ITEMS_COUNT,
+      mode: widgetSettings.mode ? widgetSettings.mode : CHART_MODES.LAUNCH_MODE,
+    });
+  }
+
+  render() {
+    const { intl } = this.props;
+    return (
+      <Fragment>
+        <FiltersControl />
+        <FieldProvider name="items">
+          <InputControl
+            fieldLabel={intl.formatMessage(messages.ItemsFiledLabel)}
+            inputWidth={ITEMS_INPUT_WIDTH}
+            type="number"
+          />
+        </FieldProvider>
+        <FieldProvider name="mode">
+          <TogglerControl
+            fieldLabel={' '}
+            items={getWidgetModeOptions(
+              [CHART_MODES.LAUNCH_MODE, CHART_MODES.TIMELINE_MODE],
+              intl.formatMessage,
+            )}
+          />
+        </FieldProvider>
+      </Fragment>
+    );
+  }
+}
